ca0d23e8c403791fbe1cd778f0c0eefac3fc3f0d
Rahul Kumar Yadav <ry573870@gmail.com>
2024-08-14 21:15:36 +0530

    Add component form devui

--- 02_Intermediate/1_Tailwind/src/App.jsx
+++ 02_Intermediate/1_Tailwind/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Card from './compnents/card'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-black p-4 rounded-xl bg-gradient-to-b from-green-400 to-blue-400'>Tailwind test</h1>
+      <h1 className='text-black p-4 rounded-xl bg-gradient-to-b from-green-400 to-blue-400 mb-4'>Tailwind test</h1>
+      <Card />
     </>
   )
 }
