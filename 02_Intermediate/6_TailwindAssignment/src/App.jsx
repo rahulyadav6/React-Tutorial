fc23e7591b10b7012f5c160644b083e5bb9ef6f0
Rahul Kumar Yadav <ry573870@gmail.com>
2024-09-04 00:24:12 +0530

    question 1 complet

--- 02_Intermediate/6_TailwindAssignment/src/App.jsx
+++ 02_Intermediate/6_TailwindAssignment/src/App.jsx
@@ -4,7 +4,8 @@ import Card from './components/Card'
 function App() {
 
   return (
-    <div className=' grid grid-cols-3 gap-4'>
+    <div className=' grid grid-cols-1 md:grid-cols-4 gap-4 '>
+      <Card name={"Rahul 12"} address={"Nepal"} />
       <Card name={"Rahul 12"} address={"Nepal"} />
       <Card name={"Rahul 12"} address={"Nepal"} />
       <Card name={"Rahul 12"} address={"Nepal"} />
--- 02_Intermediate/6_TailwindAssignment/src/components/Card.jsx
+++ 02_Intermediate/6_TailwindAssignment/src/components/Card.jsx
@@ -2,10 +2,13 @@ import Count from "./Count";
 function Card({name, address}){
     return(
         <div className="border-gray-300 shadow-lg">
-            <div className="  ">
-                <div className="bg-green-500 h-36">
-                    
+            <div className="flex flex-col flex-wrap  ">
+                <div className="bg-green-500 h-36 relative ">
+                    <div className=" absolute -bottom-12 left-20">
+                        <img className=" size-32 rounded-full" src="https://images.pexels.com/photos/27990629/pexels-photo-27990629/free-photo-of-woman-sitting-in-dress-and-with-bag.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+                    </div>
                 </div>
+                
                 <div className="bg-white h-36 flex justify-center items-center flex-col">
                     <div className=" font-bold text-xl ">
                         {name}
